6c2a3e192d7c3c25f92dc3a975836226411b581c
Ammar Arab <132087789+0ammar@users.noreply.github.com>
2026-03-30 16:00:25 +0300

    commit-25

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  allowedDevOrigins: ["192.168.10.67"],
+  allowedDevOrigins: ["192.168.8.2"],
   images: {
     remotePatterns: [
       {
